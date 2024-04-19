fff6dd55a5231b1ba9ba8f599c713a20de4e1500
Carlos Ferreira <charliefferreira@gmail.com>
2024-04-19 17:07:50 -0300

    Criação do Quiz

---
+++ questoes.js
@@ -0,0 +1,60 @@
+const questoes = [
+    {
+        id:"01",
+        enunciado: "É COM ESSA PARTE DO CORPO QUE NÓS CONSEGUIMOS IR PRA LÁ E PRA CÁ. DÁ PRA ESTICAR, DOBRAR E ATÉ CRUZAR.",
+        correta:"b",
+        alternativas:[
+            {id:"a", txt:"BRAÇO"},
+            {id:"b", txt:"PERNA"},
+            {id:"c", txt:"PESCOÇO"}
+        ],
+        img:"./imagens/perna.png"
+    },
+    {
+        id:"02",
+        enunciado: "NESSA PARTE DO CORPO É LEGAL DE VER COMO SOMOS DIFERENTES. NELA, CARREGAMOS O NOSSO CABELO DO JEITINHO QUE ELE É, NOSSOS OLHOS DE TODAS AS CORES, ALÉM DA ORELHA E DO NARIZ QUE É SÓ NOSSO.",
+        correta:"c",
+        alternativas:[
+            {id:"a", txt:"JOELHO"},
+            {id:"b", txt:"MÃO"},
+            {id:"c", txt:"CABEÇA"}
+        ],
+        img:"./imagens/cabeca.png"
+    },
+    {
+        id:"03",
+        enunciado: "ESSA É A PARTE DO CORPO QUE SE DOBRA PARA A GENTE DAR UM ABRAÇO APERTADO E É ELA QUE LIGA NOSSA MÃO AO RESTINHO DO BRAÇO.",
+        correta:"b",
+        alternativas:[
+            {id:"a", txt:"CABEÇA"},
+            {id:"b", txt:"COTOVELO"},
+            {id:"c", txt:"PESCOÇO"}
+        ],
+        img:"./imagens/cotovelo.png"
+    },
+    {
+        id:"04",
+        enunciado: "PODE FICAR ABERTA OU FECHADA, PODE DAR OI E DAR TCHAU. COM ELA PODEMOS ESCREVER, SEGURAR UM OBJETO E ATÉ FAZER CARINHO NO NOSSO PET.",
+        correta:"c",
+        alternativas:[
+            {id:"a", txt:"PESCOÇO"},
+            {id:"b", txt:"ORELHA"},
+            {id:"c", txt:"MÃO"}
+        ],
+        img:"./imagens/mao.png"
+    },
+    {
+        id:"05",
+        enunciado: "ESSA É A PARTE QUE LIGA OS NOSSOS BRAÇOS AO TRONCO. POR CAUSA DELA PODEMOS MEXER OS BRAÇOS PRA TODO LADO NA HORA DE DANÇAR E ATÉ CARREGAR A MOCHILA NA HORA DE IR PRA ESCOLA.",
+        correta:"b",
+        alternativas:[
+            {id:"a", txt:"CABEÇA"},
+            {id:"b", txt:"OMBRO"},
+            {id:"c", txt:"PESCOÇO"}
+        ],
+        img:"./imagens/ombro.png"
+    }
+];
+
+const fpositivo = "RESPOSTA CORRETA! PARABÉNS PELO EMPENHO E VAMOS PRA PRÓXIMA!";
+const fnegativo = "HMM, NÃO É BEM ISSO! QUE TAL REVISAR ESSE TEMA E ...";
